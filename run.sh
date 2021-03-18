fb1475bf46f38cfc6ad1f3c893bdb531a161555d
humofx <otahanovhumo@gmail.com>
2021-03-18 17:34:36 +0500

    changed gunicorn run

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ echo "Starting server"
 #python manage.py runserver 0.0.0.0:8000
 PROCESSOR_COUNT=$(nproc)
 GUNICORN_WORKER_COUNT=$(( PROCESSOR_COUNT * 2 + 1 ))
-gunicorn -w ${GUNICORN_WORKER_COUNT} -b 0.0.0.0:8001 elib.wsgi
+gunicorn -w ${GUNICORN_WORKER_COUNT} -b 0.0.0.0:8001 edubooksbot.wsgi
 python manage.py botpolling --username=edubooksbot
 
